69f27a112a062ad47da7e482d760725224d6cf5f
Wencey Wang <WenceyWang@dreamry.org>
2019-01-30 11:44:08 +0000

    git-tfs-id: [https://vsts.dreamry.org/DefaultCollection/]$/ToolBox;C125

---
+++ AspNet/AlertHelper/ControllerAlertExtensions.cs
@@ -0,0 +1,76 @@
+﻿using System ;
+using System . Collections ;
+using System . Collections . Generic ;
+using System . Linq ;
+
+using JetBrains . Annotations ;
+
+using Microsoft . AspNetCore . Mvc ;
+
+namespace DreamRecorder . ToolBox . AspNet . AlertHelper
+{
+
+	[PublicAPI]
+	public static class ControllerAlertExtensions
+	{
+
+		public static List <Alert> GetAlerts ( [NotNull] this Controller controller )
+		{
+			List <Alert> alerts =
+				controller . ViewData [ StringConst . Alerts ] as List <Alert> ?? new List <Alert> ( ) ;
+
+			controller . ViewData [ StringConst . Alerts ] = alerts ;
+
+			return alerts ;
+		}
+
+		public static void AddAlert ( [NotNull] this Controller controller , [NotNull] Alert alert )
+		{
+			if ( controller == null )
+			{
+				throw new ArgumentNullException ( nameof(controller) ) ;
+			}
+
+			if ( alert == null )
+			{
+				throw new ArgumentNullException ( nameof(alert) ) ;
+			}
+
+			List <Alert> alerts = GetAlerts ( controller ) ;
+
+			alerts . Add ( alert ) ;
+		}
+
+		public static void AlertError ( [NotNull] this Controller controller , string message )
+		{
+			AddAlert ( controller , new Alert ( BootstrapVariation . Danger , message ) ) ;
+		}
+
+		public static void AlertInfo ( [NotNull] this Controller controller , string message )
+		{
+			AddAlert ( controller , new Alert ( BootstrapVariation . Info , message ) ) ;
+		}
+
+		public static void AlertWarning ( [NotNull] this Controller controller , string message )
+		{
+			AddAlert ( controller , new Alert ( BootstrapVariation . Warning , message ) ) ;
+		}
+
+		public static void AlertSuccess ( [NotNull] this Controller controller , string message )
+		{
+			AddAlert ( controller , new Alert ( BootstrapVariation . Success , message ) ) ;
+		}
+
+		public static void AlertPrimary ( [NotNull] this Controller controller , string message )
+		{
+			AddAlert ( controller , new Alert ( BootstrapVariation . Primary , message ) ) ;
+		}
+
+		public static void AlertSecondary ( [NotNull] this Controller controller , string message )
+		{
+			AddAlert ( controller , new Alert ( BootstrapVariation . Secondary , message ) ) ;
+		}
+
+	}
+
+}
---
+++ AspNet/AlertHelper/StringConst.cs
@@ -0,0 +1,16 @@
+﻿using System ;
+using System . Collections ;
+using System . Collections . Generic ;
+using System . Linq ;
+
+namespace DreamRecorder . ToolBox . AspNet . AlertHelper
+{
+
+	public static class StringConst
+	{
+
+		public const string Alerts = nameof(Alerts) ;
+
+	}
+
+}
