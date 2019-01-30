fb373ae90bae85584b3d0c66bbebb9b230457563
Wencey Wang <WenceyWang@dreamry.org>
2019-01-30 16:47:35 +0000

    git-tfs-id: [https://vsts.dreamry.org/DefaultCollection/]$/ToolBox;C127

--- General/Network/SocketExtensions.cs
+++ General/Network/SocketExtensions.cs
@@ -1,13 +1,18 @@
 ﻿using System ;
 using System . Collections ;
 using System . Collections . Generic ;
+using System . IO ;
 using System . Linq ;
 using System . Net . Sockets ;
+using System . Text ;
+using System . Threading . Tasks ;
 
 using JetBrains . Annotations ;
 
 namespace DreamRecorder . ToolBox . Network
 {
+
+	
 
 	[PublicAPI]
 	public static class SocketExtensions
