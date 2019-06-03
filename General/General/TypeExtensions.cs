624bedaed88837b72916efea1ae94a5e62100042
Wencey Wang <WenceyWang@dreamry.org>
2019-06-03 22:56:27 +0000

    git-tfs-id: [https://vsts.dreamry.org/DefaultCollection/]$/ToolBox;C179

--- General/General/TypeExtensions.cs
+++ General/General/TypeExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System ;
 using System . Collections ;
 using System . Collections . Generic ;
+using System . IO ;
 using System . Linq ;
 using System . Reflection ;
 using System . Threading . Tasks ;
@@ -14,9 +15,15 @@ namespace DreamRecorder . ToolBox . General
 	public static class TypeExtensions
 	{
 
-		public static int GetInheritanceDepth ( this Type type ) => GetInheritanceDepth ( type , typeof ( object ) ) ;
+		#region GetInheritanceDepth
 
-		public static Task PrepareAssembly <T> ( ) => typeof ( T ) . Assembly . Prepare ( ) ;
+		public static int GetInheritanceDepth <T> ( ) => GetInheritanceDepth ( typeof ( T ) ) ;
+
+		public static int GetInheritanceDepth <T , TBase> ( )
+			=> GetInheritanceDepth ( typeof ( T ) , typeof ( TBase ) ) ;
+
+
+		public static int GetInheritanceDepth ( this Type type ) => GetInheritanceDepth ( type , typeof ( object ) ) ;
 
 		public static int GetInheritanceDepth ( this Type type , Type baseType )
 		{
@@ -56,16 +63,83 @@ namespace DreamRecorder . ToolBox . General
 			return count ;
 		}
 
-		public static List <PropertyInfo> GetSortedProperties ( this Type type )
+		#endregion
+
+		#region PrepareAssembly
+
+		public static Task PrepareAssembly <T> ( ) => PrepareAssembly ( typeof ( T ) ) ;
+
+		public static Task PrepareAssembly ( [NotNull] this Type type )
 		{
+			if ( type == null )
+			{
+				throw new ArgumentNullException ( nameof ( type ) ) ;
+			}
+
+			return type . Assembly . Prepare ( ) ;
+		}
+
+		#endregion
+
+
+		#region GetResourceFile
+
+		public static string GetResourceFile <T> ( string fileName ) => GetResourceFile ( typeof ( T ) , fileName ) ;
+
+		public static string GetResourceFile ( [NotNull] this Type type , [NotNull] string fileName )
+		{
+			if ( type == null )
+			{
+				throw new ArgumentNullException ( nameof ( type ) ) ;
+			}
+
+			if ( fileName == null )
+			{
+				throw new ArgumentNullException ( nameof ( fileName ) ) ;
+			}
+
+			Stream stream = type . Assembly . GetManifestResourceStream ( type , fileName ) ;
+			if ( stream != null )
+			{
+				string license ;
+
+				using ( StreamReader reader = new StreamReader ( stream ) )
+				{
+					license = reader . ReadToEnd ( ) ;
+				}
+
+				return license ;
+			}
+			else
+			{
+				return null ;
+			}
+		}
+
+		#endregion
+
+		#region GetSortedProperties
+
+		public static List <PropertyInfo> GetSortedProperties <T> ( ) => typeof ( T ) . GetSortedProperties ( ) ;
+
+		public static List <PropertyInfo> GetSortedProperties ( [NotNull] this Type type )
+		{
+			if ( type == null )
+			{
+				throw new ArgumentNullException ( nameof ( type ) ) ;
+			}
+
 			List <PropertyInfo> result = type . GetProperties ( ) .
-												Where ( prop => prop . GetCustomAttribute <IgnoreAttribute> ( )
+												Where (
+														prop
+															=> prop . GetCustomAttribute <IgnoreAttribute> ( )
 																== null ) .
 												ToList ( ) ;
 
 			Comparison <PropertyInfo> comp = ( x , y )
 												=> ( x . GetCustomAttribute <SortIndexAttribute> ( ) ? . Value
-													?? int . MaxValue ) . CompareTo ( y . GetCustomAttribute <
+													?? int . MaxValue ) . CompareTo (
+																					y . GetCustomAttribute <
 																							SortIndexAttribute> ( ) ? .
 																						Value
 																					?? int . MaxValue ) ;
@@ -75,32 +149,52 @@ namespace DreamRecorder . ToolBox . General
 			return result ;
 		}
 
-		public static List <PropertyInfo> GetSortedProperties <T> ( ) => typeof ( T ) . GetSortedProperties ( ) ;
+		#endregion
 
-		public static List <(PropertyInfo PropertyInfo , TAttribute Attribute)> GetSortedPropertiesWithAttribute
-			<TAttribute> ( this Type type ) where TAttribute : Attribute
-		{
-			return type . GetSortedPropertiesWithNullableAttribute <TAttribute> ( ) .
-						Where ( prop => prop . Attribute != null ) .
-						ToList ( ) ;
-		}
+		#region GetSortedPropertiesWithAttribute
 
 		public static List <(PropertyInfo PropertyInfo , TAttribute Attribute)> GetSortedPropertiesWithAttribute
 			<T , TAttribute> ( ) where TAttribute : Attribute
 			=> typeof ( T ) . GetSortedPropertiesWithAttribute <TAttribute> ( ) ;
 
-		public static List <(PropertyInfo , TAttribute Attribute)> GetSortedPropertiesWithNullableAttribute
-			<TAttribute> ( this Type type ) where TAttribute : Attribute
+		public static List <(PropertyInfo PropertyInfo , TAttribute Attribute)> GetSortedPropertiesWithAttribute
+			<TAttribute> ( [NotNull] this Type type ) where TAttribute : Attribute
 		{
-			return type . GetSortedProperties ( ) .
-						Select ( prop => ( (PropertyInfo PropertyInfo , TAttribute Attribute) )
-									( prop , prop . GetCustomAttribute <TAttribute> ( true ) ) ) .
+			if ( type == null )
+			{
+				throw new ArgumentNullException ( nameof ( type ) ) ;
+			}
+
+			return type . GetSortedPropertiesWithNullableAttribute <TAttribute> ( ) .
+						Where ( prop => prop . Attribute != null ) .
 						ToList ( ) ;
 		}
+
+		#endregion
+
+		#region GetSortedPropertiesWithNullableAttribute
 
 		public static List <(PropertyInfo PropertyInfo , TAttribute Attribute)> GetSortedPropertiesWithNullableAttribute
 			<T , TAttribute> ( ) where TAttribute : Attribute
 			=> typeof ( T ) . GetSortedPropertiesWithNullableAttribute <TAttribute> ( ) ;
+
+		public static List <(PropertyInfo , TAttribute Attribute)> GetSortedPropertiesWithNullableAttribute
+			<TAttribute> ( [NotNull] this Type type ) where TAttribute : Attribute
+		{
+			if ( type == null )
+			{
+				throw new ArgumentNullException ( nameof ( type ) ) ;
+			}
+
+			return type . GetSortedProperties ( ) .
+						Select (
+								prop
+									=> ( (PropertyInfo PropertyInfo , TAttribute Attribute) )
+									( prop , prop . GetCustomAttribute <TAttribute> ( true ) ) ) .
+						ToList ( ) ;
+		}
+
+		#endregion
 
 	}
 
