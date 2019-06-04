feb362a2443cc89f609913f9da525259d711069a
Wencey Wang <WenceyWang@dreamry.org>
2019-06-04 15:18:28 +0000

    git-tfs-id: [https://vsts.dreamry.org/DefaultCollection/]$/ToolBox;C182

--- General/General/StaticServiceContainer.cs
+++ General/General/StaticServiceContainer.cs
@@ -1,28 +1,39 @@
-﻿using System ;
-using System . Collections ;
-using System . Collections . Generic ;
-using System . Linq ;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System . Threading ;
 
-using Microsoft . Extensions . DependencyInjection ;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace DreamRecorder . ToolBox . General
+namespace DreamRecorder.ToolBox.General
 {
 
-	public static class StaticServiceProvider
-	{
+    public static class StaticServiceProvider
+    {
 
-		public static IServiceProvider Provider { get ; private set ; }
+        public static IServiceProvider Provider { get; private set; }
 
-		public static IServiceCollection ServiceCollection { get ; set ; } = new ServiceCollection ( ) ;
+        public static IServiceCollection ServiceCollection { get; set; } = new ServiceCollection();
 
-		public static void Update ( )
-		{
-			lock ( ServiceCollection )
+        public static void Update()
+        {
+            try
+            {
+                lock (ServiceCollection)
+                {
+                    Provider = ServiceCollection.BuildServiceProvider(true);
+                }
+
+            }
+            catch (Exception e)
 			{
-				Provider = ServiceCollection . BuildServiceProvider ( true ) ;
+                Thread.Yield();
+				Thread . Sleep ( 1 ) ;
+				Update ( ) ;
 			}
-		}
+        }
 
-	}
+    }
 
 }
