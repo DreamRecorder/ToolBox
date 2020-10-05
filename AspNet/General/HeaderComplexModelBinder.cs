88f62c1b69c13f19185be6a6b25ee23421aa290a
Wencey Wang <WenceyWang@dreamry.org>
2020-10-05 01:57:19 +0000

    git-tfs-id: [https://vsts.dreamry.org/DefaultCollection/]$/ToolBox;C543

---
+++ AspNet/General/HeaderComplexModelBinder.cs
@@ -0,0 +1,67 @@
+﻿using System ;
+using System . Collections ;
+using System . Collections . Generic ;
+using System . Linq ;
+using System . Text . Json ;
+using System . Threading . Tasks ;
+
+using JetBrains . Annotations ;
+
+using Microsoft . AspNetCore . Mvc ;
+using Microsoft . AspNetCore . Mvc . ModelBinding ;
+
+namespace DreamRecorder . ToolBox . AspNet . General
+{
+
+	public class HeaderComplexModelBinder : IModelBinder
+	{
+
+		public Task BindModelAsync ( ModelBindingContext bindingContext )
+		{
+			if ( bindingContext == null )
+			{
+				throw new ArgumentNullException ( nameof ( bindingContext ) ) ;
+			}
+
+			string headerKey = bindingContext . ModelMetadata . BinderModelName ?? bindingContext . FieldName ;
+
+			if ( ! string . IsNullOrEmpty ( headerKey ) )
+			{
+				string headerValue =
+					bindingContext . HttpContext . Request . Headers [ headerKey ] . FirstOrDefault ( ) ;
+
+				if ( ! string . IsNullOrEmpty ( headerValue ) )
+				{
+					Type modelType = bindingContext . ModelMetadata . ModelType ;
+
+					bindingContext . Model  = JsonSerializer . Deserialize ( headerValue , modelType ) ;
+					bindingContext . Result = ModelBindingResult . Success ( bindingContext . Model ) ;
+
+					return Task . CompletedTask ;
+				}
+			}
+
+			bindingContext . Result = ModelBindingResult . Failed ( ) ;
+
+			return Task . CompletedTask ;
+		}
+
+		public static void EnableHeaderComplexModelBinder (
+			[NotNull]
+			MvcOptions options ,
+			[CanBeNull]
+			Action <MvcOptions> next = null )
+		{
+			if ( options == null )
+			{
+				throw new ArgumentNullException ( nameof ( options ) ) ;
+			}
+
+			options . ModelBinderProviders . Insert ( 0 , new HeaderComplexModelBinderProvider ( ) ) ;
+
+			next ? . Invoke ( options ) ;
+		}
+
+	}
+
+}
---
+++ AspNet/General/HeaderComplexModelBinderProvider.cs
@@ -0,0 +1,47 @@
+﻿using System ;
+using System . Collections ;
+using System . Collections . Generic ;
+using System . Linq ;
+
+using Microsoft . AspNetCore . Mvc ;
+using Microsoft . AspNetCore . Mvc . ModelBinding ;
+using Microsoft . AspNetCore . Mvc . ModelBinding . Binders ;
+using Microsoft . AspNetCore . Mvc . ModelBinding . Metadata ;
+
+namespace DreamRecorder . ToolBox . AspNet . General
+{
+
+	public class HeaderComplexModelBinderProvider : IModelBinderProvider
+	{
+
+		public IModelBinder GetBinder ( ModelBinderProviderContext context )
+		{
+			if ( context == null )
+			{
+				throw new ArgumentNullException ( nameof ( context ) ) ;
+			}
+
+			if ( context . Metadata . IsComplexType )
+			{
+				if ( context . Metadata is DefaultModelMetadata metadata )
+				{
+					object headerAttribute =
+						metadata . Attributes . Attributes . FirstOrDefault (
+																			a
+																				=> a . GetType ( )
+																					== typeof ( FromHeaderAttribute
+																					) ) ;
+
+					if ( headerAttribute != null )
+					{
+						return new BinderTypeModelBinder ( typeof ( HeaderComplexModelBinder ) ) ;
+					}
+				}
+			}
+
+			return null ;
+		}
+
+	}
+
+}
