a14fc8d138193ae6695dcb6168e2e1a0d96046e7
Wencey Wang <WenceyWang@dreamry.org>
2021-01-17 15:09:07 +0000

    git-tfs-id: [https://vsts.dreamry.org/DefaultCollection/]$/ToolBox;C641

---
+++ CommandLine/TelegramBot/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System ;
+using System . Collections . Generic ;
+using System . Linq ;
+using System . Text ;
+using System . Threading . Tasks ;
+
+using DreamRecorder . ToolBox . CommandLine ;
+
+namespace DreamRecorder . ToolBox . TelegramBot
+{
+
+	public class TelegramBot
+	{
+
+		//public ITelegramBotClient BotClient { get; set; }
+
+		//public ISessionProvider SessionProvider { get; private set; }
+
+		//public ICommandProvider CommandProvider { get; private set; }
+
+		//public IUserDataProvider UserDataProvider { get; set; }
+
+		//public ITelegramIdUserProvider UserProvider { get; set; }
+
+		//public IUserPermissionProvider PermissionProvider { get; set; }
+
+	}
+
+	public abstract class TelegramBotProgramBase
+		<T , TExitCode , TSetting , TSettingCategory> : ProgramBase <T , TExitCode , TSetting , TSettingCategory>
+		where T : TelegramBotProgramBase <T , TExitCode , TSetting , TSettingCategory>
+		where TExitCode : ProgramExitCode <TExitCode> , new ( )
+		where TSetting : SettingBase <TSetting , TSettingCategory> , new ( )
+		where TSettingCategory : Enum , IConvertible
+	{
+
+	}
+
+}
