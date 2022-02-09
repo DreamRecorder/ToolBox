1c207c87e658bf80cd2e06b3b57b4ea380b037b3
Wencey Wang <WenceyWang@dreamry.org>
2022-02-09 07:41:18 +0000

    git-tfs-id: [https://vsts.dreamry.org/DefaultCollection/]$/ToolBox;C769

--- General/General/PredicateExtensions.cs
+++ General/General/PredicateExtensions.cs
@@ -8,9 +8,17 @@ namespace DreamRecorder . ToolBox . General ;
 public static class PredicateExtensions
 {
 
-	public static Predicate<T> Or<T>(params Predicate<T>[] predicates) => item => predicates . Any ( predicate => predicate ( item ) ) ;
+	public static Predicate <T> Pass <T> ( ) => ( T _ ) => true ;
 
+	public static Predicate <T> Reject <T> ( ) => ( T _ ) => false ;
 
-	public static Predicate<T> And<T>(params Predicate<T>[] predicates) => item => predicates . All ( predicate => predicate ( item ) ) ;
+	public static Predicate <T> Not <T> ( Predicate <T> predicate )
+		=> ( item ) => ! predicate ( item ) ;
+
+	public static Predicate <T> Or <T> ( params Predicate <T> [ ] predicates )
+		=> item => predicates . Any ( predicate => predicate ( item ) ) ;
+
+	public static Predicate <T> And <T> ( params Predicate <T> [ ] predicates )
+		=> item => predicates . All ( predicate => predicate ( item ) ) ;
 
 }
