135c5a3d5e81c06e4a072d02ed6adfc2f6c8cd89
Wencey Wang <WenceyWang@dreamry.org>
2022-06-16 12:21:11 +0000

    git-tfs-id: [https://vsts.dreamry.org/DefaultCollection/]$/ToolBox;C866

---
+++ General/General/EnumeratorExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System ;
+using System . Collections ;
+using System . Collections . Generic ;
+using System . Linq ;
+
+namespace DreamRecorder . ToolBox . General ;
+
+public static class EnumeratorExtensions
+{
+	public static IEnumerable<T> ToIEnumerable<T>(IEnumerator<T> enumerator)
+	{
+		while (enumerator.MoveNext())
+		{
+			yield return enumerator.Current;
+		}
+	}
+
+}
