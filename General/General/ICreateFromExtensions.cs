a04dae8f5405c2b6e2e3b768c954fa55a91233de
Wencey Wang <WenceyWang@dreamry.org>
2022-09-06 13:20:05 +0000

    git-tfs-id: [https://vsts.dreamry.org/DefaultCollection/]$/ToolBox;C945

---
+++ General/General/ICreateFromExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System ;
+using System . Collections ;
+using System . Collections . Generic ;
+using System . Linq ;
+
+namespace DreamRecorder . ToolBox . General ;
+
+public static class ICreateFromExtensions
+{
+
+	public static IEnumerable <T> CreateFromList <T , TSource> ( this IEnumerable <TSource> sources )
+		where T : ICreateFrom <T , TSource>
+		=> sources . Select ( T . CreateFrom ) ;
+
+}
