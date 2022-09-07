bffefd2c6be954e38d2417d57d8b3676c1542913
Wencey Wang <WenceyWang@dreamry.org>
2022-09-07 21:34:52 +0000

    git-tfs-id: [https://vsts.dreamry.org/DefaultCollection/]$/ToolBox;C947

--- General/General/ExceptionHelper.cs
+++ General/General/ExceptionHelper.cs
@@ -9,6 +9,31 @@ namespace DreamRecorder . ToolBox . General
 	public static class ExceptionHelper
 	{
 
+		public static bool Success ( this Action action )
+		{
+			try
+			{
+				action ( ) ;
+				return true ;
+			}
+			catch
+			{
+				return false ;
+			}
+		}
+
+		public static T IgnoreException <T> ( this Func <T> func , T onError = default )
+		{
+			try
+			{
+				return func ( ) ;
+			}
+			catch
+			{
+				return onError ;
+			}
+		}
+
 		public static ArgumentException XmlNameMismatch ( string argumentName , Type type )
 			=> new ArgumentException ( ExceptionMessages . XmlNameMismatch ( argumentName , type ) , argumentName ) ;
 
