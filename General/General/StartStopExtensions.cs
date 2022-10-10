b37b1456fe54014168c70ba9e5d7f78f4c5e6b92
Wencey Wang <WenceyWang@dreamry.org>
2022-10-10 11:05:40 +0000

    git-tfs-id: [https://vsts.dreamry.org/DefaultCollection/]$/ToolBox;C970

---
+++ General/General/StartStopExtensions.cs
@@ -0,0 +1,80 @@
+﻿using System ;
+using System . Collections ;
+using System . Collections . Generic ;
+using System . Linq ;
+using System . Reflection ;
+
+using JetBrains . Annotations ;
+
+namespace DreamRecorder . ToolBox . General ;
+
+public static class StartStopExtensions
+{
+
+	public static void StartMembers ( [NotNull] this IStartStop startStop )
+	{
+		if ( startStop == null )
+		{
+			throw new ArgumentNullException ( nameof ( startStop ) ) ;
+		}
+
+		List <PropertyInfo> readable = startStop . GetType ( ) .
+													GetProperties ( ) .
+													Where (
+															p
+																=> p . CanRead
+																	&& ( ! p . GetAccessors ( ) .
+																				Any ( m => m . IsStatic ) ) ) .
+													ToList ( ) ;
+
+		foreach ( PropertyInfo propertyInfo in readable )
+		{
+			if (ExceptionHelper.IgnoreException(() => propertyInfo.GetValue(startStop)) is IStartStop ss)
+			{
+				ss . Start ( ) ;
+			}
+		}
+	}
+
+	public static void StopMembers([NotNull] this IStartStop startStop)
+	{
+		if (startStop == null)
+		{
+			throw new ArgumentNullException(nameof(startStop));
+		}
+
+		List<PropertyInfo> readable = startStop.GetType().
+												GetProperties().
+												Where(
+													p
+														=> p.CanRead
+															&& (!p.GetAccessors().
+																	Any(m => m.IsStatic))).
+												ToList();
+
+		foreach (PropertyInfo propertyInfo in readable)
+		{
+			if (ExceptionHelper.IgnoreException(() => propertyInfo.GetValue(startStop)) is IStartStop ss)
+			{
+				ss.Stop();
+			}
+		}
+	}
+
+	public static void StartAll ( this IEnumerable <IStartStop> startStops )
+	{
+		foreach ( IStartStop startStop in startStops )
+		{
+			startStop . Start ( ) ;
+		}
+	}
+
+	public static void StopAll ( this IEnumerable <IStartStop> startStops )
+	{
+		foreach ( IStartStop startStop in startStops )
+		{
+			startStop . Stop ( ) ;
+		}
+	}
+
+}
---
+++ UnitTest/StartStopTests.cs
@@ -0,0 +1,113 @@
+﻿using System ;
+using System . Collections ;
+using System . Collections . Generic ;
+using System . Linq ;
+
+using DreamRecorder . ToolBox . General ;
+
+using Microsoft . VisualStudio . TestTools . UnitTesting ;
+
+namespace DreamRecorder . ToolBox . UnitTest ;
+
+[TestClass]
+public class StartStopTests
+{
+
+	public class StatefulStartStopSelfStartStopTestStub : IStatefulStartStop
+	{
+
+		public string Name { get ; set ; }
+
+		bool IStatefulStartStop . IsRunningStatus { get ; set ; }
+
+		public void StartOverride ( )
+		{
+			Console . WriteLine ( $"{Name} start." ) ;
+			( this as IStartStop ) . Start ( ) ;
+		}
+
+		public void StopOverride ( )
+		{
+			Console . WriteLine ( $"{Name} stop." ) ;
+			( this as IStartStop ) . Stop ( ) ;
+		}
+
+	}
+
+	public class StatefulStartStopMemberStartStopTestStub : IStatefulStartStop
+	{
+
+		public IStartStop Member { get ; set ; }
+
+		public IStartStop Exception => throw null ;
+
+		public string Name { get ; set ; }
+
+		bool IStatefulStartStop . IsRunningStatus { get ; set ; }
+
+		public void StartOverride ( )
+		{
+			Console . WriteLine ( $"{Name} start." ) ;
+			this . StartMembers ( ) ;
+		}
+
+		public void StopOverride ( )
+		{
+			Console . WriteLine ( $"{Name} stop." ) ;
+			this . StopMembers ( ) ;
+		}
+
+	}
+
+	[TestMethod]
+	public void SelfStartStopTest ( )
+	{
+		IStartStop stub = new StatefulStartStopSelfStartStopTestStub { Name = nameof ( stub ) } ;
+		stub . Start ( ) ;
+		stub . Stop ( ) ;
+		stub . Start ( ) ;
+		stub . Stop ( ) ;
+	}
+
+	[TestMethod]
+	public void LoopMemberStartStopTest ( )
+	{
+		StatefulStartStopMemberStartStopTestStub stub1 =
+			new StatefulStartStopMemberStartStopTestStub { Name = nameof ( stub1 ) } ;
+		StatefulStartStopMemberStartStopTestStub stub2 =
+			new StatefulStartStopMemberStartStopTestStub { Name = nameof ( stub2 ) } ;
+
+		stub1 . Member = stub2 ;
+		stub2 . Member = stub1 ;
+
+		IStartStop stub = stub1 ;
+
+		stub . Start ( ) ;
+		stub . Stop ( ) ;
+		stub . Start ( ) ;
+		stub . Stop ( ) ;
+
+		stub . Start ( ) ;
+		stub = stub2 ;
+		stub . Stop ( ) ;
+		stub . Start ( ) ;
+		stub . Stop ( ) ;
+	}
+
+	[TestMethod]
+	public void SelfMemberStartStopTest ( )
+	{
+		StatefulStartStopMemberStartStopTestStub stub1 =
+			new StatefulStartStopMemberStartStopTestStub { Name = nameof ( stub1 ) } ;
+
+		stub1 . Member = stub1 ;
+
+		IStartStop stub = stub1 ;
+
+		stub . Start ( ) ;
+		stub . Stop ( ) ;
+		stub . Start ( ) ;
+		stub . Stop ( ) ;
+	}
+
+}
